1b746ca2986f17efa8c440dd77a83b09b42ee7ec
Hasnahfiroz <116860660+Hasnahfiroz@users.noreply.github.com>
2023-02-15 10:41:29 +0530

    Add files via upload

---
+++ CO3/main2.py
@@ -0,0 +1,12 @@
+import graphics3
+from graphics3 import circle,rectangle
+from graphics3.tdgraphics import cuboid,sphere
+from graphics3.circle import*
+print("area of circle:",circle.area_circle(10))
+print("perimeter of circle:",circle.perimeter_circle(10))
+print("area of rectangle:",rectangle.area_rec(10,2))
+print("perimeter of rectangle:",rectangle.perimeter_rec(10,2))
+print("area of cuboid:",cuboid.area_cuboid(10,2,4))
+print("perimeter of cuboid:",cuboid.perimeter_cuboid(10,2,4))
+print("area of sphere:",sphere.area_sphere(10))
+print("perimeter of sphere:",sphere.perimeter_sphere(10))
